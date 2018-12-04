a468adff36f038a893947165230fdbd948565641
Harry Zhan <tianhz3@uw.edu>
2018-12-03 22:13:17 -0800

    still need work

--- server.R
+++ server.R
@@ -1,9 +1,22 @@
 ## install.packages("maps")
 ## install.packages("mapproj")
+## install.packages(c("ggplot2", "ggmap", "mapdata"))
+## install.packages("devtools")
+devtools::install_github("dkahle/ggmap", ref = "tidyup")
 library(maps)
+library(ggplot2)
+library(ggmap)
+library(mapdata)
 library(mapproj)
 function(input, output) {
   output$map <- renderPlot({
-    ## percent_map()
+    wa_df <- subset(map_data("state"), region == "washington")
+    wa_county <- subset(map_data("county"), region == "washington")
+    wa_base <- ggplot(data = wa_df, mapping = aes(x = long, y = lat, group = group)) + 
+      coord_fixed(1.3) + 
+      geom_polygon(color = "black", fill = "gray") +
+      theme_nothing() + 
+      geom_polygon(data = wa_county, fill = NA, color = "white") +
+      geom_polygon(color = "black", fill = NA)
   })
 }
--- ui.R
+++ ui.R
@@ -4,23 +4,26 @@ library(shiny)
 library(dplyr)
 census <- read.csv("./data/stco-mr2010-1.csv", stringsAsFactors = FALSE)
 gender <- select(census, STNAME, CTYNAME, SEX, RESPOP)
-male <- filter(gender, SEX == 1)
-male_county <- group_by(male, STNAME) %>%
-  summarize(ppl = sum(RESPOP))
-female <- filter(gender, SEX == 2)
-female_county <- group_by(female, STNAME) %>%
-  summarize(ppl = sum(RESPOP))
+gender_wa <- filter(gender, STNAME == "Washington")
+male <- filter(gender_wa, SEX == 1)
+male_county <- group_by(male, CTYNAME) %>%
+  summarize(ppl_m = sum(RESPOP))
+female <- filter(gender_wa, SEX == 2)
+female_county <- group_by(female, CTYNAME) %>%
+  summarize(ppl_f = sum(RESPOP))
+gender_wa_county <- right_join(male_county,female_county, by = "CTYNAME") %>%
+  mutate(ppl = ppl_m + ppl_f) %>%
+  mutate(m_perc = ppl_m / ppl) %>%
+  mutate(f_perc = ppl_f / ppl)
+wa_county <- as.list(gender_wa_county$CTYNAME)
 fluidPage(
-  titlePanel("Gender Distribution of the United States"),   
+  titlePanel("Gender Distribution of Washington State"),   
   sidebarLayout (
     sidebarPanel(
-      selectInput("var",
-                  label = "which gender?",
-                  choices = c("Male", "Female"),
-                  selected = "Male"), 
-      sliderInput("range", 
-                  label = "Range of interest:",
-                  min = 0, max = 100, value = c(0, 100))
+      selectInput("countyname",
+                  label = "Which county?",
+                  choices = wa_county),
+    radioButtons("genderoption", "Which gender?", c("Male", "Female"))
     ),
     mainPanel(plotOutput("map"))
   )
