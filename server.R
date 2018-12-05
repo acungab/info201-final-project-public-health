e86599d15bc603d843d2326ea1b21239b26e908b
iGuest <iguest@IS-M082L23-13.clients.uw.edu>
2018-12-04 23:39:51 -0800

    nearly finished

--- server.R
+++ server.R
@@ -1,4 +1,6 @@
 library(ggplot2)
+library(plotrix)
+source("./ui.R")
 function(input, output) {
   output$bar <- renderPlot({
     county <- filter(wa, CTYNAME == input$countyname) %>%
@@ -9,9 +11,37 @@ function(input, output) {
                    "30-34", "35-39", "40-44", "45-49", "50-54", "55-59",
                    "60-64", "65-69", "70-74", "75-79", "80-84", "85+")
     ggplot(age_data, aes(x=factor(AGEGRP), y=RESPOP)) +
-      geom_bar(stat="identity", fill="blue") +
-      geom_text(aes(label=RESPOP), vjust=1.6, color="white", size=3) +
+      geom_bar(stat="identity", fill="cornflowerblue") +
+      geom_text(aes(label=RESPOP), vjust=-0.4, size=3) +
       theme_minimal() + xlab("Age group (years)") + ylab("Population") +
       scale_x_discrete(labels = age_group) + theme_bw()
     })
+  output$genderpie <- renderPlot({
+    county <- filter(wa, CTYNAME == input$countyname) %>%
+      filter(IMPRACE == input$raceoption)
+    gender_data <- group_by(county, SEX) %>%
+      summarize(RESPOP = sum(RESPOP))
+    gender_slice <- as.vector(gender_data$RESPOP)
+    gender_pct <- round(gender_slice/sum(gender_slice)*100, digits = 2)
+    gender_group <- paste0(c("Male: ", "Female: "), gender_pct, "%")
+    pie3D(gender_slice, labels = gender_group, col=c("green", "pink"), explode=0.1)
+  })
+  output$gendertext <- renderText({
+    county <- filter(wa, CTYNAME == input$countyname) %>%
+      filter(IMPRACE == input$raceoption)
+    gender_data <- group_by(county, SEX) %>%
+      summarize(RESPOP = sum(RESPOP))
+    paste0("In general, there are ", gender_data[1, 2], " males and ", 
+           gender_data[2, 2], " females of corresponding race in ", input$countyname, ".")
+  })
+  output$ethnicpie <- renderPlot({
+    county <- filter(wa, CTYNAME == input$countyname) %>%
+      filter(IMPRACE == input$raceoption)
+    ethnic_data <- group_by(county, ORIGIN) %>%
+      summarize(RESPOP = sum(RESPOP))
+    ethnic_slice <- as.vector(ethnic_data$RESPOP)
+    ethnic_pct <- round(ethnic_slice/sum(ethnic_slice)*100, digits = 2)
+    ethnic_group <- paste0(c("Not Hispanic: ", "Hispanic: "), ethnic_pct, "%")
+    pie3D(ethnic_slice, labels = ethnic_group, col=c("azure", "brown3"))
+  })
 }
--- ui.R
+++ ui.R
@@ -1,13 +1,15 @@
-## install.packages("dplyr")
+
 ## install.packages("shiny")
+## install.packages("plotrix")
 library(shiny)
 library(dplyr)
 census <- read.csv("./data/stco-mr2010-1.csv", stringsAsFactors = FALSE)
 wa <- filter(census, STNAME == "Washington")
-wa_county <- as.data.frame(table(wa$CTYNAME), stringsAsFactors = FALSE) 
+wa_county <- as.data.frame(table(wa$CTYNAME), stringsAsFactors = FALSE)
 wa_county <- as.list(wa_county$Var1)
+print(wa_county)
 fluidPage(
-  titlePanel("Gender Distribution of Washington State"),   
+  titlePanel("Racial analysis of Washington State"),   
   sidebarLayout (
     sidebarPanel(
       selectInput("countyname",
@@ -15,15 +17,19 @@ fluidPage(
                   choices = wa_county),
       radioButtons("raceoption", "Race?",
                          c("White" = 1, "Black or African Amercian" = 2,
-                                "American Indian and Alaska Native" = 3, "Asian" = 4,
-                                "Native Hawaiian and Other Pacific Islander" = 5) ##, 
-                           ## "interracial" = 6:31)
+                            "American Indian and Alaska Native" = 3, "Asian" = 4,
+                            "Native Hawaiian and Other Pacific Islander" = 5, 
+                           "White + Black or African American" = 6,
+                           "White + Asian" = 8,
+                           "Black or African American + Asian" = 11)
                          )
     ),
     mainPanel(
-      plotOutput("bar"),
-      plotOutput("genderpie"),
-      plotOutput("ethnicpie")
+      tabsetPanel(
+        tabPanel("Age distribution", plotOutput("bar")),
+        tabPanel("Gender distribution", plotOutput("genderpie"), textOutput("gendertext")),
+        tabPanel("Ethnic distribution", plotOutput("ethnicpie"), textOutput("ethnictext"))
+      )
       )
   )
 )
