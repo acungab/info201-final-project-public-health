913ae02104007710fb6412e133ee43f428220c6f
Harry Zhan <tianhz3@uw.edu>
2018-12-05 12:22:05 -0800

    finished harry and caio

--- server.R
+++ server.R
@@ -1,7 +1,115 @@
 library(ggplot2)
 library(plotrix)
-source("./ui.R")
+source("ui.R")
 function(input, output) {
+  age_label <- c("0-4", "5-9","10-14","15-19","20-24","25-29","30-34","35-39","40-44","45-49",
+                 "50-54", "55-59", "60-64","65-69","70-74","75-79",
+                 "80-84","85 <=")    
+  
+  
+  
+  DataMale <- function(stateInput){
+    listM <- list()
+    for(i in 1:18){
+      bigD <- filter(census, STNAME == stateInput)
+      m <- filter(bigD, SEX == 1)
+      m <- filter(m, AGEGRP == i)
+      male_pop <- sum(m$RESPOP)
+      listM[i] <- male_pop
+    }
+    dfM <- data.frame(matrix(unlist(listM), nrow=18, byrow=T))
+    names(dfM)<- c("Population")
+    dfM$Age <- age_label     
+    return(dfM)
+  }
+  
+  DataFemale <- function(stateInput){
+    listF <- list()
+    for(i in 1:18){
+      bigD <- filter(census, STNAME == stateInput)
+      f <- filter(bigD, SEX == 2)
+      f <- filter(f, AGEGRP == i)
+      female_pop <- sum(f$RESPOP)
+      listF[i] <- female_pop
+    }
+    dfF <- data.frame(matrix(unlist(listF), nrow=18, byrow=T))
+    names(dfF)<- c("Population")
+    dfF$Age <- age_label     
+    return(dfF)
+  }
+  
+  output$map <- renderPlot({
+    
+    stateInput <- input$state
+    dfF <- DataFemale(stateInput)
+    dfM <- DataMale(stateInput)
+    
+    
+    if(input$g == "Bar Graph"){
+      dfT <- as.data.frame(c(1:18))
+      dfT$Population <- dfM$Population + dfF$Population
+      dfT$INDEX <- c(1:18)
+      dfT$Age <- age_label
+      
+      
+      chart1 <- ggplot(data = dfT, aes(x = reorder(Age,INDEX),y = Population)) +
+        geom_bar(stat = "identity", width = .8, fill = "cornflowerblue") +
+        geom_text(aes(label = Population), vjust = -.4, size = 3) +
+        theme_minimal() + xlab("Age group (years)") + ylab("Population") +
+        ggtitle(paste("Age Distribution of",input$state)) + theme_bw()
+    }
+    
+    if(input$g == "Population Pyramid"){
+      dfM$Population <- dfM$Population * -1
+      gender_age <- bind_rows(dfM,dfF)
+      gender_age$sex <- c(replicate(18,"MALE"), replicate(18,"FEMALE"))
+      gender_age$Age <- c("0-4", "5-9","10-14","15-19","20-24","25-29","30-34","35-39","40-44","45-49",
+                          "50-54", "55-59", "60-64","65-69","70-74","75-79",
+                          "80-84","85 <=")
+      
+      gender_age$INDEX <- c(1:36)
+      
+      if(max(dfF$Population) < 700001){
+        brks <- seq(-150000, 150000, 50000)
+        lbls <-paste0(as.character(c(seq(150, 0, -50), seq(50, 150, 50))), "k")
+      }else if(max(dfF$Population > 700000)){
+        brks <- seq(-1500000, 1500000, 500000)
+        lbls <-paste0(as.character(c(seq(1.5, 0, -.5), seq(.5, 1.5, .5))), "m")
+      }
+      chart1 <- ggplot(gender_age, aes(x = reorder(Age, INDEX), y = Population, fill = sex)) +   # Fill column
+        geom_bar(subset = .(sex == "FEMALE"),stat = "identity", width = .8) +   # draw the bars
+        geom_bar(subset = .(sex == "MALE"),stat = "identity", width = .8) +   # draw the bars
+        scale_y_continuous(breaks = brks,   # Breaks
+                           labels = lbls) + # Labels
+        coord_flip() +  # Flip axes
+        labs(title=paste0("Age Distribution and Gender of ", input$state)) +
+        # theme_tufte() +  # Tufte theme from ggfortify
+        theme(plot.title = element_text(hjust = .5),
+              axis.ticks = element_blank()) +   # Centre plot title
+        scale_fill_brewer(palette = "green1") + # Color palette
+        xlab("Age group (years)") + geom_text(aes(label = Population), vjust = 0, size = 3) + theme_bw()
+    }
+    
+    chart1
+  })
+  
+  output$message <- renderText({
+    
+    stateInput <- input$state
+    dfM <- DataMale(stateInput)
+    dfF <- DataFemale(stateInput)
+    tot <- round(sum(dfM$Population)/(sum(dfM$Population)+sum(dfF$Population)), digits = 3) * 100
+    
+    paste0("There are approximately ",sum(dfM$Population)  ," males and ", sum(dfF$Population),
+           " females in the state of ", input$state, " thefore we can see that ", tot ,
+           "% of all citiezens are male in the state of ", input$state,
+           ". The age group for males with the largest number of people is between ",
+           dfM[which.max(dfM$Populatio),2], "years old and in that group there is ",
+           dfM[which.max(dfM$Populatio),1], " people. On the other hand, the biggest age group for females is between ",
+           dfF[which.max(dfF$Populatio),2], " years old containing ",dfF[which.max(dfF$Populatio),1], 
+           " people.")
+  })
+  
   output$bar <- renderPlot({
     county <- filter(wa, CTYNAME == input$countyname) %>%
       filter(IMPRACE == input$raceoption)
--- ui.R
+++ ui.R
@@ -1,35 +1,69 @@
-## install.packages("ggplot2")
-## install.packages("dplyr")
-## install.packages("plotrix")
+## install.packages("plyr")
+library(plyr)
 library(shiny)
+library(ggplot2)
 library(dplyr)
-census <- read.csv("./data/stco-mr2010-1.csv", stringsAsFactors = FALSE)
+
+census <- read.csv("./data/stco-mr2010-1.csv", stringsAsFactors = F)
+usa_states <- as.data.frame(table(census$STNAME), stringsAsFactors = F)
+usa_states <- as.list(usa_states$Var1)
+
 wa <- filter(census, STNAME == "Washington")
 wa_county <- as.data.frame(table(wa$CTYNAME), stringsAsFactors = FALSE)
 wa_county <- as.list(wa_county$Var1)
-print(wa_county)
-fluidPage(
-  titlePanel("Racial analysis of Washington State"),   
-  sidebarLayout (
-    sidebarPanel(
-      selectInput("countyname",
-                  label = "Which county?",
-                  choices = wa_county),
-      radioButtons("raceoption", "Race?",
-                         c("White" = 1, "Black or African Amercian" = 2,
-                            "American Indian and Alaska Native" = 3, "Asian" = 4,
-                            "Native Hawaiian and Other Pacific Islander" = 5, 
-                           "White + Black or African American" = 6,
-                           "White + Asian" = 8,
-                           "Black or African American + Asian" = 11)
-                         )
-    ),
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Age distribution", plotOutput("bar")),
-        tabPanel("Gender distribution", plotOutput("genderpie"), textOutput("gendertext")),
-        tabPanel("Ethnic distribution", plotOutput("ethnicpie"), textOutput("ethnictext"))
-      )
-      )
+
+
+ui <- fluidPage(
+  titlePanel("Population Overview in the United States"),
+  navbarPage("",
+             tabPanel("Age and gender analysis",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("state",
+                                      label = "Which State?",
+                                      choices = usa_states)
+                        ),
+                        column(4, offset = 2, 
+                               radioButtons("g",
+                                            "Graphs",
+                                            c("Population Pyramid","Bar Graph")
+                               )
+                               
+                        )
+                      ),
+                      mainPanel(
+                        plotOutput("map", width = "150%", height = "500px"),
+                        column(12, offset = 3,
+                               textOutput("message")
+                        )
+                      )
+             ),
+             tabPanel("Racial analysis of Washington State",
+                      sidebarLayout (
+                        sidebarPanel(
+                          selectInput("countyname",
+                                      label = "Which county?",
+                                      choices = wa_county),
+                          radioButtons("raceoption", "Race?",
+                                       c("White" = 1, "Black or African Amercian" = 2,
+                                         "American Indian and Alaska Native" = 3, "Asian" = 4,
+                                         "Native Hawaiian and Other Pacific Islander" = 5, 
+                                         "White + Black or African American" = 6,
+                                         "White + Asian" = 8,
+                                         "Black or African American + Asian" = 11)
+                          )
+                        ),
+                        mainPanel(
+                          tabsetPanel(
+                            tabPanel("Age distribution", plotOutput("bar")),
+                            tabPanel("Gender distribution", plotOutput("genderpie"), textOutput("gendertext")),
+                            tabPanel("Ethnic distribution", plotOutput("ethnicpie"), textOutput("ethnictext"))
+                          )
+                        )
+                      )
+             )
   )
 )
+
+
+
